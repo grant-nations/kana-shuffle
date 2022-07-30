760a3b5fb710398cc6549ed7b584c16aed092595
GRANT NATIONS <u1247784@umail.utah.edu>
2022-07-30 11:27:04 -0600

    add japanese otf font files and basic table stucture

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const kana = require("./utils/kana");
 
 let fonts = {
-    Roboto: {
-        normal: 'fonts/Roboto-Regular.ttf'
+    NotoSansJP: {
+        normal: './fonts/NotoSansJP-Regular.otf'
     }
 };
 let PdfPrinter = require("pdfmake");
@@ -11,7 +11,20 @@ let printer = new PdfPrinter(fonts);
 let fs = require("fs");
 
 let docDefinition = {
-
+    content: [
+        {
+            table: {
+                body:[
+                    ["one", "two", "three", "four"],
+                    ["5", "6", "7", "8"],
+                    ["11", "12", "13", "14"]
+                ]
+            }
+        }
+    ],
+    defaultStyle: {
+        font: "NotoSansJP"
+    }
 };
 let pdfDoc = printer.createPdfKitDocument(docDefinition, {});
 pdfDoc.pipe(fs.createWriteStream('document.pdf'));
