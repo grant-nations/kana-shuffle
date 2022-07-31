bda77514489be6a25e66b41f327d893b9296a71a
GRANT NATIONS <u1247784@umail.utah.edu>
2022-07-30 22:53:49 -0600

    setup node file serving

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const express = require("express");
 const {createPdf, types} = require("./utils/createPdf");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/hiragana", (req, res) => {
     createPdf(types.HIRAGANA, (binary) => {
@@ -30,6 +34,11 @@ app.get("/romaji", (req, res) => {
     }, (error) => {
         res.send("ERROR: " + error);
     });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
